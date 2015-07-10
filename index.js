9f754631b4afce24f7d8f14b0060fff7284df9d6
Sam Verschueren <sam.verschueren@gmail.com>
2015-07-10 23:05:49 +0200

    Bound to localhost IP

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World 2\n');
-}).listen(8080);
-console.log('Server running at http://0.0.0.0:8080/');
+  res.end('Hello World\n');
+}).listen(8080, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:8080/');
