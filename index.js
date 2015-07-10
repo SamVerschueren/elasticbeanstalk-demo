70c8ead8a18f6f3f5da1768f912bb2bccff2fa8e
Sam Verschueren <sam.verschueren@gmail.com>
2015-07-10 22:46:51 +0200

    removed ip

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(8080, '0.0.0.0');
+}).listen(8080);
 console.log('Server running at http://0.0.0.0:8080/');
